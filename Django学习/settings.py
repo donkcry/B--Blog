fa8ab961bee0b6af3ed34147158f2a3cd97184a9
donkcry <2839788640@qq.com>
2026-03-24 20:29:56 +0800

    B--最终终待上线

--- Django学习/settings.py
+++ Django学习/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rz(cpe&6&-t7k97c%2@xw(@eey_ua!zwv4b37ezgm810jxu9mw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
